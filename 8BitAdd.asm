68891c226b5d049c79a0ca5a6220c2509662fc18
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2023-11-11 15:12:36 +0530

    Update 8BitAdd.asm

--- 8BitAdd.asm
+++ 8BitAdd.asm
@@ -1,46 +1,72 @@
-data segment
-num1 db ?
-num2 db ?
-sum db ?
-msg1 db 10,13,"Enter the first number: $"
-msg2 db 10,13,"Enter the second number: $"
-msg3 db 10,13,"Sum is: $"
-data ends
-code segment
-assume cs:code,ds:data
-start: mov ax,data
- mov ds,ax
- lea dx,msg1
- mov ah,9h
- int 21h
- mov ah,1h
- int 21h
- sub al,30h
- mov num1,al
- lea dx,msg2
- mov ah,9h
- int 21h
- mov ah,1h
- int 21h
- sub al,30h
- mov num2,al
- add al,num1
- mov sum,al
- mov ah,0
- aaa
- add ah,30h
- add al,30h
- mov bx,ax
- lea dx,msg3
- mov ah,9h
- int 21h
- mov ah,2h
- mov dl,bh
- int 21h
- mov ah,2
- mov dl,bl
- int 21h
- mov ah,4ch
- int 21h
-code ends
-end start
+ASSUME DS: DATA , CS: CODE
+DATA SEGMENT
+    MSG1 DB 13,10, "ENTER FIRST NUMBER: $"
+    MSG2 DB 13,10, "ENTER SECOND NUMBER: $" 
+    MSG3 DB 13,10, "SUM: $"
+DATA ENDS
+
+CODE SEGMENT
+    START: MOV AX, DATA
+           MOV DS, AX
+
+           LEA DX, MSG1
+           MOV AH, 09H
+           INT 21H
+
+           MOV AH, 01H
+           INT 21H
+           MOV BH, AL 
+           MOV AH, 01H
+           INT 21H 
+           MOV BL, AL
+
+           LEA DX, MSG2
+           MOV AH, 09H
+           INT 21H
+
+           MOV AH, 01H
+           INT 21H
+           MOV CH, AL
+           MOV AH, 01H
+           INT 21H
+           MOV CL, AL
+
+           MOV AL,BL
+           MOV AH, 00H ; CLEAR
+           ADD AL,CL
+           AAA
+
+           ADD AX, 3030H
+           MOV BL, AL ; LOWER VALUE OF RESULT
+
+           MOV AL, AH ; MOVE CARRY TO AL BEFORE AAA
+
+           MOV AH, 00H;  CLEAR
+           ADD AL, BH
+           ADD AL, CH
+           AAA
+           ADD AX, 3030H
+           MOV BH, AL
+           MOV CL, AH
+
+           LEA DX, MSG3
+           MOV AH, 09H
+           INT 21H ; PRINT "SUM: "
+
+           MOV DL, CL
+           MOV AH, 02H
+           INT 21H ; PRINT CARRY
+
+           MOV DL, BH
+           MOV AH, 02H
+           INT 21H ; PRINT NEXT DIGIT
+
+           MOV DL, BL
+           MOV AH, 02H
+           INT 21H ; PRINT LAST DIGIT
+
+           MOV AH, 4CH
+           INT 21H
+
+    CODE ENDS
+    END START
