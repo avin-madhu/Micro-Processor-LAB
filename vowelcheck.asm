85934863b6937e6547f0bae6a40211fb0ed77707
Avin Madhu <84233047+avin-madhu@users.noreply.github.com>
2023-11-13 21:40:15 +0530

    Update vowelcheck.asm

--- vowelcheck.asm
+++ vowelcheck.asm
@@ -1,46 +1,55 @@
-.MODEL SMALL
-.DATA
-inp db 0ah,0dh,'enter the string: ','$'
-msg db 0ah,0dh,'the number of vowels: ','$'
-vowel db 'a','A','e','E','i','I','o','O','u','U','$'
-databuf db 100,0,100 dup('$')
-.code
-START:
- mov ax,@data
- mov ds,ax
- 
- lea dx,inp
- mov ah,09h
- int 21h
- 
- lea dx,databuf
- mov ah,0ah
- int 21h
- mov si,dx
- 
- lea dx,msg
- mov ah,09h
- int 21h
-       
- mov bl,00h
-check:  lea di,vowel
- mov cx,000ah
- mov al,[si]
-cont:
- cmp al,[di]
- je found
- inc di
- loop cont
- jmp next
-found:  inc bl
-next:   inc si
- cmp databuf[si],0ah
- jne check
- mov dl,bl
- add dl,30h
- mov ah,02h
- int 21h
- 
+ASSUME DS: DATA, CS: CODE
 
-.EXIT
+DATA SEGMENT
+    INP DB 0AH, 0DH, 'ENTER THE STRING: $'
+    MSG DB 0AH, 0DH, 'THE NUMBER OF VOWELS: $'
+    VOWEL DB 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U', '$'
+    DATABUF DB 100, 0, 100 DUP('$')
+DATA ENDS
+
+CODE SEGMENT
+    START:
+        MOV AX, DATA
+        MOV DS, AX
+
+        LEA DX, INP
+        MOV AH, 09H
+        INT 21H
+
+        LEA DX, DATABUF
+        MOV AH, 0AH
+        INT 21H
+        MOV SI, DX
+
+        LEA DX, MSG
+        MOV AH, 09H
+        INT 21H
+
+        MOV BL, 00H
+    CHECK:
+        LEA DI, VOWEL
+        MOV CX, 000AH
+        MOV AL, [SI]
+    CONT:
+        CMP AL, [DI]
+        JE FOUND
+        INC DI
+        LOOP CONT
+        JMP NEXT
+    FOUND:
+        INC BL
+    NEXT:
+        INC SI
+        CMP DATABUF[SI], 0AH
+        JNE CHECK
+
+        MOV DL, BL
+        ADD DL, 30H
+        MOV AH, 02H
+        INT 21H
+
+        MOV AH, 4CH
+        INT 21H
+
+CODE ENDS
 END START
